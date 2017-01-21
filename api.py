b279e460b92761921d187b47171c256e86b8089c
nitinsurya <ubuntu@ip-172-31-38-80.us-east-2.compute.internal>
2017-01-21 17:31:39 +0000

    updates

--- api.py
+++ api.py
@@ -25,24 +25,37 @@ def root():
 @app.route('/sample_text', methods=['GET'])
 def sample_text():
   return make_response(jsonify({'text': 'Hello world'}))
- 
+
+@app.route('/create_action', methods=['POST'])
+def create_action():
+  global acc_base_url, c, conn
+  account_id = '57f89267360f81f104543bd1'
+  data = request.get_json()
+  c.execute("insert into transactions(`name`, `product_id`, `product_name`, `product_imgurl`, `merchant_id`, `merchant_name`, `date_time`, `product_desc`, `amount`) values (?,?,?,?,?,?,?,?,?)", data['name'], data['pid'], data['pname'], data['img'], data['mid'], data['mname'], data['date'], data['pdesc'], data['amount'])
+  conn.commit()
+  return make_response(jsonify({}))
 
 @app.route('/make_transaction', methods=['GET'])
 def make_transaction():
   global acc_base_url
   account_id, out_vals = '57f89267360f81f104543bd1', []
   merchant_id = request.args.get('merchant_id')
+  action_id = request.args.get('transaction_id')
   if merchant_id is not None:
     url = acc_base_url + account_id + "/purchases?key=" + pennhack_key
     purchase_details = {"merchant_id": merchant_id, "medium": "balance", "purchase_date": "2017-01-21", "amount": 0.01, "description": "string"}  
     json_data = requests.post(url, data = json.dumps(purchase_details), headers = {'Content-Type': 'application/json'}).json()
+    #update_action_row(action_id, json_data, account_id)
 
     return make_response(jsonify(json_data))
   else:
-    jsonify({"error": "Merchant ID not given"}, 404)
+    return make_response(jsonify({"error": "Merchant ID not given"}, 404))
 
-def create_action_request():
-  pass
+def update_action_row(action_id, co_transaction_id, payer_id):
+  global c, conn
+  c.execute("update transactions set transaction_id = '?' and approver_id = '?' where id = ?", co_transaction_id, payer_id, action_id)
+  conn.commit()
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
